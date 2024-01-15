13e454c301e5a493a9b36fdf03fd5b6a2e2744b2
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-01-15 17:52:46 +0100

    Build data with docker

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Maps layers
+# Maps layers
+
+## Build data
+
+```
+docker-compose -f docker-compose-tools.yaml run --rm tools ./low_emission_zone.sh
+```
--- data/low_emission_zone.sh
+++ data/low_emission_zone.sh
@@ -3,8 +3,8 @@
 set -e
 
 curl "https://overpass.kumi.systems/api/interpreter?data=%5Bout%3Axml%5D%5Btimeout%3A360%5D%3B%0Arelation%5B%22boundary%22%3D%22low_emission_zone%22%5D%2834.016241889667036%2C-10.014225844127038%2C59.84481485969108%2C14.902766343372946%29%3B%0Aout%20geom%3B" > low_emission_zone.osm.xml
-echo 'y' | npx osmtogeojson
+npm install -g osmtogeojson
 npx osmtogeojson low_emission_zone.osm.xml > low_emission_zone.geojson
-# ogr2ogr low_emission_zone-f.geojson -where "start_date IS NULL OR start_date < '`date --iso`'" low_emission_zone.geojson 
+# ogr2ogr low_emission_zone-f.geojson -where "start_date IS NULL OR start_date < '`date --iso`'" low_emission_zone.geojson
 tippecanoe low_emission_zone.geojson --force -o low_emission_zone.mbtiles
 rm low_emission_zone.osm.xml low_emission_zone.geojson
